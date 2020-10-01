3e4621fbdd662b10c81c769a772490ed051fd77d
diloki <dinakirilova@gmail.com>
2020-10-01 21:53:51 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+if (input.lightLevel() < 2) {
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.SmallHeart)
+} else {
+    basic.clearScreen()
+}
+basic.forever(function () {
+	
+})
